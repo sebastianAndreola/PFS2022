a084013015824c17421fbd013092fe14b5da5e77
sebastianAndreola <105524947+sebastianAndreola@users.noreply.github.com>
2022-05-26 19:45:26 -0300

    Delete tablademultiplicacion.js

--- 1-PB/Clase_3/tablademultiplicacion.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var rls = require("readline-sync");
-var nroTabla = rls.questionFloat("Ingrese el numero : ");
-var hastaNro = rls.questionFloat("Ingrese hasta que numero : ");
-var contador = 1;
-for (contador; contador <= hastaNro; contador++)
-    console.log("".concat(nroTabla, " x ").concat(contador, " ="), nroTabla * contador);
